0197adf572cabe87a4b3103dbefdd8b4ad2af2d3
Stepan Hruda <stepan.hruda@gmail.com>
2015-03-27 10:39:50 -0400

    Initial Commit

---
+++ simulator-installer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  simulator-installer
+//
+//  Created by Stepan Hruda on 3/27/15.
+//  Copyright (c) 2015 ShopKeep. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
