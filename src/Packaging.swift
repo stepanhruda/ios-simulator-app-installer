da1c2c1c21c69e8ecaa1e68abe94ed77a4f45b97
Stepan Hruda <stepan.hruda@gmail.com>
2015-03-29 17:49:23 -0400

    Respect app's name, playing with functional shenanigans

--- src/Packaging.swift
+++ src/Packaging.swift
@@ -1,25 +1,45 @@
 func packageApp(appPath: String, deviceIdentifier: String) {
-    let validPath =
-    appPath
+    let validPath = appPath
         |> getFullPath
-        |> flatMap(validateFileExistence)
+        >>= validateFileExistence(fileManager: NSFileManager.defaultManager())
     
     if let validPath = validPath {
+        
+        let appName = (validPath
+            |> URL
+            >>= lastPathComponent
+            >=> deletePathExtension)! // There's a better way to do this than force unwrap
+        
         let share = "/usr/local/share/"
         system("xcodebuild -project \(share)app-package-launcher/app-package-launcher.xcodeproj \"PACKAGED_APP=\(validPath)\" \"TARGET_DEVICE=\(deviceIdentifier)\" > /dev/null")
-        system("rm -rf \"App Installer.app\"")
-        system("mv \(share)app-package-launcher/build/Release/app-package-launcher.app \"App Installer.app\"")
+        system("rm -rf \"\(appName) Installer.app\"")
+        system("mv \(share)app-package-launcher/build/Release/app-package-launcher.app \"\(appName) Installer.app\"")
         system("rm -rf \(share)app-package-launcher/build")
-        println("App successfully packaged to \"App Installer.app\"")
+        
+        println("\(appName) successfully packaged to \"\(appName) Installer.app\"")
     } else {
+        
         println("Provided .app not found at \"\(appPath)\"")
+        
     }
 }
 
 func getFullPath(path: String) -> String? {
-    return NSURL(fileURLWithPath: path)?.path
+    return URL(path)?.path
 }
 
-func validateFileExistence(path: String) -> String? {
-    return NSFileManager.defaultManager().fileExistsAtPath(path) ? path : nil
+func validateFileExistence(#fileManager: NSFileManager)(path: String) -> String? {
+    return fileManager.fileExistsAtPath(path) ? path : nil
+}
+
+func lastPathComponent(#url: NSURL) -> String? {
+    return url.lastPathComponent
+}
+
+func URL(path: String) -> NSURL? {
+    return NSURL.fileURLWithPath(path)
+}
+
+func deletePathExtension(path: String) -> String? {
+    return path.stringByDeletingPathExtension
 }
