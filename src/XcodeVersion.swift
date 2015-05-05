e055c849f9053b604e085705c7b65dc729e14ffd
nataliq <npacovska@fmi.uni-sofia.bg>
2015-05-05 13:30:40 +0300

    Fix Xcode version check to detect versions higher than 6.3

--- src/XcodeVersion.swift
+++ src/XcodeVersion.swift
@@ -3,9 +3,17 @@ import Foundation
 let RequiredXcodeVersion = "Xcode 6.3"
 
 func isRequiredXcodeIsInstalled() -> Bool {
-    return currentXcodeVersion() == RequiredXcodeVersion
+    
+    if let comparationResult = currentXcodeVersion()?.localizedCaseInsensitiveCompare(RequiredXcodeVersion) {
+        switch comparationResult {
+        case .OrderedDescending, .OrderedSame: return true
+        default: ()
+        }
+    }
+    return false
 }
 
 func currentXcodeVersion() -> String? {
     return run("xcodebuild -version").first
 }
+
