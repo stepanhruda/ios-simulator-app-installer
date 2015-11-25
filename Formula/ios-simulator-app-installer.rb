daaecdc8b564efb918b2055c3518b787c882c280
Teng Siong Ong <siong1987@gmail.com>
2015-11-25 11:14:47 -0800

    Consolidate formula on the same repo.

---
+++ Formula/ios-simulator-app-installer.rb
@@ -0,0 +1,19 @@
+class IosSimulatorAppInstaller < Formula
+  homepage "https://github.com/stepanhruda/ios-simulator-app-installer"
+  url "https://github.com/siong1987/ios-simulator-app-installer.git",
+    :revision => "532a8ed86388e5080fcadf176ba55a0f9fc2d237"
+  version "0.2.0"
+
+  depends_on :xcode => "7"
+  depends_on :macos => :yosemite
+
+  def install
+    system "make"
+    bin.install "ios-simulator-app-installer"
+    share.install "app-package-launcher"
+  end
+
+  test do
+    system "make"
+  end
+end
